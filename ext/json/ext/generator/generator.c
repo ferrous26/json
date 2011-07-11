11ed55d58a8b99955bdb108b6df41dde44d265cf
Mark Rada <mrada@marketcircle.com>
2011-07-11 00:35:31 -0400

    Switch CLASS_OF with TYPE for checking type when generating collections
    This should allow NSString et al. to be detected properly.

--- ext/json/ext/generator/generator.c
+++ ext/json/ext/generator/generator.c
@@ -875,12 +875,12 @@ static void generate_json_float(FBuffer *buffer, VALUE Vstate, JSON_Generator_St
 static void generate_json(FBuffer *buffer, VALUE Vstate, JSON_Generator_State *state, VALUE obj)
 {
     VALUE tmp;
-    VALUE klass = CLASS_OF(obj);
-    if (klass == rb_cHash) {
+    VALUE type = TYPE(obj);
+    if (type == T_HASH) {
         generate_json_object(buffer, Vstate, state, obj);
-    } else if (klass == rb_cArray) {
+    } else if (type == T_ARRAY) {
         generate_json_array(buffer, Vstate, state, obj);
-    } else if (klass == rb_cString) {
+    } else if (type == T_STRING) {
         generate_json_string(buffer, Vstate, state, obj);
     } else if (obj == Qnil) {
         generate_json_null(buffer, Vstate, state, obj);
@@ -888,11 +888,11 @@ static void generate_json(FBuffer *buffer, VALUE Vstate, JSON_Generator_State *s
         generate_json_false(buffer, Vstate, state, obj);
     } else if (obj == Qtrue) {
         generate_json_true(buffer, Vstate, state, obj);
-    } else if (klass == rb_cFixnum) {
+    } else if (type == T_FIXNUM) {
         generate_json_fixnum(buffer, Vstate, state, obj);
-    } else if (klass == rb_cBignum) {
+    } else if (type == T_BIGNUM) {
         generate_json_bignum(buffer, Vstate, state, obj);
-    } else if (klass == rb_cFloat) {
+    } else if (type == T_FLOAT) {
         generate_json_float(buffer, Vstate, state, obj);
     } else if (rb_respond_to(obj, i_to_json)) {
         tmp = rb_funcall(obj, i_to_json, 1, Vstate);
